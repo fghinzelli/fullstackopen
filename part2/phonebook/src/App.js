c0010c9b469a616f0f3fe6de056d06dc58b51aa4
Fernando Ghinzelli <fghinzelli@gmail.com>
2022-11-28 11:20:44 -0300

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+  const [filteredPersons, setFilteredPersons] = useState([...persons]);
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNewName = event => {
     event.preventDefault();
@@ -15,16 +20,26 @@ const App = () => {
       if ((persons.filter(person => person.name === formattedName)).length > 0) {
         alert(`${formattedName} is already added to phonebook`)
       } else {
-        setPersons(persons.concat({ name: newName, number: newNumber }))
+        const newPersons = persons.concat({ name: newName, number: newNumber })
+        setPersons(newPersons)
+        setFilteredPersons(newPersons.filter(person => (person.name.toUpperCase()).indexOf(filter.toUpperCase()) !== -1));
         setNewName('');
         setNewNumber('');
       }
     }
   }
 
+  const handleFilterByName = event => {
+    const txtFilter = event.target.value;
+    setFilter(txtFilter);
+    setFilteredPersons(persons.filter(person => (person.name.toUpperCase()).indexOf(txtFilter.toUpperCase()) !== -1));
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      filter shown with <input value={filter} onChange={handleFilterByName} type="text" />
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
@@ -38,7 +53,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {filteredPersons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )

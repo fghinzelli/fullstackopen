c27bbbb04479e32e0ca1f553e0e0341e56a6162e
Fernando Ghinzelli <fghinzelli@gmail.com>
2023-01-05 11:53:58 -0300

    Exercises 2.19 and 2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Filter from './components/Filter';
+import Notification from './components/Notifications';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 import personsApi from './services/persons';
@@ -10,6 +11,8 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null);
+  const [messageType, setMessageType] = useState(null);
 
   useEffect(()=> {
     personsApi.getAll()
@@ -18,6 +21,17 @@ const App = () => {
         setFilteredPersons(response.data)
       })
   }, [])
+
+  const handleShowMessage = (msg, type) => {
+    const TIMEOUT_MESSAGE = 5 * 1000;
+
+    setMessage(msg)
+    setMessageType(type)
+    setTimeout(() => {
+      setMessage(null)
+      setMessageType(null)
+    }, TIMEOUT_MESSAGE);
+  }
 
   const handleNewName = event => {
     event.preventDefault();
@@ -32,10 +46,19 @@ const App = () => {
       let filtredList = persons.filter(person => person.name === newPerson.name)
       if (filtredList.length > 0) {
         if (window.confirm(`${newPerson.name} is already added to phonebook, replace the old number with a new one?`)) {
+          let newList = persons.filter(person => person.name !== newPerson.name)
           personsApi.update(filtredList[0].id, newPerson)
           .then(updatedPerson => {
-            const newList = persons.filter(person => person.name !== newPerson.name).concat(updatedPerson.data)
+            newList = newList.concat(updatedPerson.data)
             updateLists(newList)
+            handleShowMessage(`Updated ${newPerson.name}`, 'success')
+          }).catch(error => {
+            if (error.response.status === 404) {
+              updateLists(newList)
+              handleShowMessage(`Information of ${newPerson.name} has already been removed from server`, 'error')
+            } else {
+              handleShowMessage(error.message, 'error')
+            }
           })
         }
       } else {
@@ -43,6 +66,7 @@ const App = () => {
         .then(response => {
           const newPersons = persons.concat(response.data)
           updateLists(newPersons)
+          handleShowMessage(`Added ${newPerson.name}`, 'success')
         })
       }
     }
@@ -61,6 +85,13 @@ const App = () => {
       .then(response => {
         setPersons(persons.filter(p => p.id !== person.id))
         setFilteredPersons(persons.filter(p => p.id !== person.id))
+      }).catch(error => {
+        console.log(error)
+        if (error.response.status === 404) {
+          handleShowMessage(`Information of ${person.name} has already been removed from server`, 'error')
+        } else {
+          handleShowMessage(error.message, 'error')
+        }
       })
     }
   }
@@ -74,6 +105,7 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
+      <Notification message={message} type={messageType} />
       <Filter 
         filter={filter} 
         filterByName={handleFilterByName} 
---
+++ part2/phonebook/src/components/Notifications.js
@@ -0,0 +1,15 @@
+import './Notification.css';
+
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return (
+    <div className={`message ${type}`} >
+      {message}
+    </div>
+  )
+}
+
+export default Notification;

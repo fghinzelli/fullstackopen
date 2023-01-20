903c958158bd10f583648faf08d2b15f150272dc
Fernando Ghinzelli <fghinzelli@gmail.com>
2023-01-20 09:24:08 -0300

    Exercise 3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const persons = [
+let persons = [
   {
     "id": 1,
     "name": "Arto Hellas",
@@ -40,6 +40,12 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).send('Página não encontrada');
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+  response.status(204).end();
 })
 
 app.get('/info', (request, response) => {

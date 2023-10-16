a1d11eaa1ed550be5c8c09ba9ccf0e7ec40f9a7c
Fernando Ghinzelli <fghinzelli@gmail.com>
2023-10-16 14:37:24 -0300

    Exercise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fghinzelli:${password}@cluster0.bqwai0s.mongodb.net/personApp?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length<5) {
+  Person.find({}).then(result => {
+    console.log('phonebook:');
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close()
+  })
+} else {
+  const newName = process.argv[3];
+  const newNumber = process.argv[4]
+  const person  = new Person({
+    name: newName,
+    number: newNumber
+  });
+  
+  person.save().then(result => {
+    console.log(`added ${newName} number ${newNumber} to phonebook`);
+    mongoose.connection.close();
+  })
+}
+
+
+
+

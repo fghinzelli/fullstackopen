497a5b3b6d0fc0448504bb5c75eadd08e65de00d
Fernando Ghinzelli <fghinzelli@gmail.com>
2023-10-16 16:49:46 -0300

    Exercises 3.13 to .14

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
@@ -13,33 +15,13 @@ app.use(cors())
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-];
 
-const SERVER_PORT = process.env.PORT || 3001 ;
+const SERVER_PORT = process.env.PORT;
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(people => {
+    response.json(people);
+  })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -48,42 +30,35 @@ app.post('/api/persons', (request, response) => {
   
   if (!body.name || !body.number) {
     return response.status(400).json({ error: 'Content missing' })
-  } else {
-    const person = persons.find(person => person.name === body.name)
-    if (person) {
-      return response.status(400).json({error: 'The Name already exists'})
-    }
-    const newPerson = {
-      id: Math.round((Math.random() * 100) + 4),
-      ...body
-    }
-    persons = persons.concat(newPerson);
-    response.status(201).json(newPerson);
-  }
+  } 
+    
+  const newPerson = new Person({...body})
+
+  newPerson.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = parseInt(request.params.id);
-  const person = persons.find(person => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).send('Página não encontrada');
-  }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  });
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = parseInt(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-  response.status(204).end();
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => response.status(204).end())
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
   let now = new Date();
-  response.write(`Phonebook has info for ${persons.length} people\n\n`)
-  response.write(now.toString());
-  response.end();
+  Person.find({}).then(persons => {
+    response.write(`Phonebook has info for ${persons.length} people\n\n`)
+    response.write(now.toString());
+    response.end();
+  }).catch(error =>  response.write('Erro ao buscar os dados: ', error.message))
+  
 })
 
 app.listen(SERVER_PORT, () => {

15facbbc93b4c5349aaa96cb2eb69781acf895c5
Fernando Ghinzelli <fghinzelli@gmail.com>
2023-10-17 11:44:35 -0300

    Error handling

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -15,7 +15,6 @@ app.use(cors())
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-
 const SERVER_PORT = process.env.PORT;
 
 app.get('/api/persons', (request, response) => {
@@ -39,10 +38,19 @@ app.post('/api/persons', (request, response) => {
   })
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  });
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404)
+      next()
+    }
+  })
+  .catch(error => {
+    next(error)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -60,6 +68,25 @@ app.get('/info', (request, response) => {
   }).catch(error =>  response.write('Erro ao buscar os dados: ', error.message))
   
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+
+app.use(errorHandler)
+
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listen on port ${SERVER_PORT}`)
